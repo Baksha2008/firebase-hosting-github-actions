c28c2e5abf8ceed4cdb7b5953f6b7ffbcc91bc0e
Baksha2008 <baksha2008@gmail.com>
2021-06-09 15:40:06 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -147,7 +147,7 @@ function App() {
     return <p>loading...</p>;
   }
 
-  console.log("bbb", process.env.REACT_APP_SECRET_KEY);
+  console.log("aaa", process.env.REACT_APP_SECRET_KEY);
 
   return (
     <div className="App">
